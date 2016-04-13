805cf691f2a9f5bab18cbc0027a196372b22e829
Tampler6 <arkadia81@gmail.com>
2016-04-13 16:54:59 +0300

    Git Commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -47,7 +47,7 @@ Route::group(['middleware' => ['web']], function () {
             "uses" => "Closure\Closure@closure",
         ]
     );
-    
+    //// GIT TESTS
     Route::get('/tasks', 'TaskController@index');
     Route::post('/task', 'TaskController@store');
     Route::delete('/task/{task}', 'TaskController@destroy');
